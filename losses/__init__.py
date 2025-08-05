5d0a7b87548569d54d3c8740d93332ab5916d5c1
Qifeng Wang <wangqifeng0702@gmail.com>
2025-08-05 20:11:53 +0100

    Delete losses directory

--- losses/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dice_loss import *
-from .mesh_loss import *
-from .meshlossclass import Mesh_loss
-from pytorch3d.loss import chamfer_distance,mesh_laplacian_smoothing, mesh_normal_consistency, mesh_edge_loss
